c235571ca30a75c7b3389d76eba77a7e51f226cd
banjaminlee <banjaminlee0212@gmail.com>
2024-12-12 01:16:04 -0800

    first commit

---
+++ Linkedin_auto_connector_bot.py
@@ -0,0 +1,217 @@
+"""
+This script is a LinkedIn bot that automatically sends connection requests with a custom note to profiles on LinkedIn.
+It uses the Selenium WebDriver to navigate LinkedIn and interact with the UI elements.
+Do 100 requests per week!!!!!
+If not, LinkedIn will block your account.
+Add my LinkedIn also - https://www.linkedin.com/in/mrbondarenko/
+Replace your search link with keywords you need!
+Go to LinkedIn main page, press on the search bar, put the keywords you need(Tech Recruter or Cloud Engineer for example),
+press enter, select people only! copy the link and paste it in the SEARCH_LINK variable.
+ Have fun!
+"""
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, MoveTargetOutOfBoundsException, ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Replace with your LinkedIn credentials
+LINKEDIN_USERNAME = '#@yahoo.com' # your email
+LINKEDIN_PASSWORD = '' # your password
+
+MAX_RETRIES = 5  # Maximum number of retries for refreshing
+
+SEARCH_LINK = ("https://www.linkedin.com/search/results/people/?geoUrn=%5B%22103644278%22%5D&keywords=technical%20recruiter&origin=GLOBAL_SEARCH_HEADER&page=14")
+# Base connection message template
+BASE_CONNECTION_MESSAGE = """Hi there,
+
+I hope this message finds you well. I'm exploring new opportunities in tech and would love to connect. I have a strong background in Azure Cloud and Software Development, with a focus on Cloud Engineering and AI/ML.
+
+Looking forward to connecting!
+
+Best regards,
+Pavlo Bondarenko
+"""
+
+MAX_CONNECT_REQUESTS = 20  # Limit for connection requests
+
+def login_to_linkedin(driver, username, password):
+    try:
+        driver.get("https://www.linkedin.com/login")
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "username")))
+
+        # Enter username
+        username_field = driver.find_element(By.ID, "username")
+        username_field.send_keys(username)
+
+        # Enter password
+        password_field = driver.find_element(By.ID, "password")
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.RETURN)
+        time.sleep(5)  # Wait for the page to load or enter captcha
+        WebDriverWait(driver, 20).until(EC.url_contains("/feed"))
+        logging.info("Successfully logged into LinkedIn.")
+        time.sleep(5)  # Wait for the feed to load
+    except Exception as e:
+        logging.error(f"Error during LinkedIn login: {e}")
+
+def go_to_next_page(driver):
+    try:
+        time.sleep(5)  # Wait for the page to load
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # Scroll down
+        next_page_button = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@aria-label='Next']"))
+        )
+        next_page_button.click()
+        logging.info("Navigated to the next page")
+        time.sleep(5)  # Wait for the new page to load
+    except NoSuchElementException as e:
+        logging.error(f"Element not found: {e}")
+        return False
+    except Exception as e:
+        logging.error(f"Error navigating to the next page: {e}")
+        return False
+    return True
+
+def scrool_down(driver):
+    try:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # Scroll down
+        time.sleep(5)  # Wait for the page to load
+    except Exception as e:
+        logging.error(f"Error during scrolling down: {e}")
+
+def handle_connect_button_with_retry(driver, button):
+    retry_count = 0
+    while retry_count < MAX_RETRIES:
+        try:
+            button.click()
+            time.sleep(2)
+
+            add_note_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//button[@aria-label='Add a note']"))
+            )
+            add_note_button.click()
+            time.sleep(2)
+
+            message_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//textarea[@name='message']"))
+            )
+            message_box.send_keys(BASE_CONNECTION_MESSAGE)
+            time.sleep(2)
+
+            send_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'artdeco-button__text') and text()='Send']"))
+            )
+            send_button.click()
+            logging.info("Sent connection request with a custom note.")
+            time.sleep(2)
+            return  # Exit the function if successful
+        except ElementClickInterceptedException as e:
+            logging.error(f"Error: Element not clickable, retrying... {e}")
+            retry_count += 1
+            if not refresh_page(driver, retry_count):  # Try refreshing the page
+                logging.error("Unable to resolve the error after retries. Exiting.")
+                break
+        except Exception as e:
+            logging.error(f"Error handling 'Connect' button: {e}")
+            break
+
+def handle_follow_button(button):
+    try:
+        button.click()
+        logging.info("Followed the user.")
+        time.sleep(1)
+    except Exception as e:
+        logging.error(f"Error handling 'Follow' button: {e}")
+
+def process_buttons(driver):
+    try:
+        # Navigate to the search page
+        driver.get(SEARCH_LINK)
+        scrool_down(driver)
+        time.sleep(5)
+
+        connect_requests_sent = 0
+
+        working = True
+
+
+        while working:
+            # Find all buttons on the page
+            buttons = driver.find_elements(By.TAG_NAME, "button")
+
+            # Count "Connect" and "Follow" buttons
+            connect_buttons_count = sum(1 for button in buttons if button.text.strip().lower() == "connect")
+            follow_buttons_count = sum(1 for button in buttons if button.text.strip().lower() == "follow")
+            logging.info(f"Total 'Connect' buttons on the page: {connect_buttons_count}")
+            logging.info(f"Total 'Follow' buttons on the page: {follow_buttons_count}")
+
+            # Process each "Connect" and "Follow" button
+            for button in buttons:
+                button_text = button.text.strip().lower()
+                if button_text == "connect" and connect_requests_sent < MAX_CONNECT_REQUESTS:
+                    handle_connect_button_with_retry(driver, button)
+                    connect_requests_sent += 1
+                    if connect_requests_sent >= MAX_CONNECT_REQUESTS:
+                        logging.info(
+                            f"Reached the limit of {MAX_CONNECT_REQUESTS} connection requests. Stopping connection requests.")
+                        working = False
+                        break
+                    time.sleep(5)
+                elif button_text == "follow":
+                    handle_follow_button(button)
+                    time.sleep(5)
+
+            # Attempt to navigate to the next page
+            if not go_to_next_page(driver):
+                logging.info("No more pages to process. Exiting.")
+                break
+
+            # Scroll down to load all elements on the new page
+            scrool_down(driver)
+            time.sleep(5)
+
+    except Exception as e:
+        logging.error(f"Error while processing buttons: {e}")
+
+
+def refresh_page(driver, retries):
+    for attempt in range(1, retries + 1):
+        try:
+            logging.info(f"Attempt {attempt}/{retries}: Refreshing the page.")
+            driver.refresh()  # Refresh the page
+            time.sleep(5)  # Wait for the page to reload
+            return True
+        except Exception as e:
+            logging.error(f"Error during page refresh: {e}")
+
+        if attempt == retries:
+            logging.error("Maximum retries reached. Exiting the program.")
+            driver.quit()
+            exit(1)
+    return False
+
+
+if __name__ == "__main__":
+    options = Options()
+    options.binary_location = 'C:/Program Files/Mozilla Firefox/firefox.exe' ## path to your firefox browser(must install firefox browser)
+
+    # Set up the webdriver (Replace the path with the path to your webdriver) // mine is geckodriver32.exe already installed in the directory
+    # go to https://github.com/mozilla/geckodriver/releases to download latest version of geckodriver
+    service = Service('geckodriver32.exe')
+    driver = webdriver.Firefox(service=service, options=options)
+
+    try:
+        login_to_linkedin(driver, LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
+        process_buttons(driver)
+    finally:
+        driver.quit()
---
+++ README.md
@@ -0,0 +1,133 @@
+<div style="text-align: center;">
+    <a href="https://www.youtube.com/channel/UCk2O3jSU3_B2MMGr8wLcAdw" target="_blank" title="CodeVoyage YouTube Channel">
+        <img src="banner.jpg" alt="Alt text">
+    </a>
+</div>
+
+# 🤖 LinkedIn Auto Connector Bot
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+## 🌟 Overview
+
+The LinkedIn Auto Connector Bot is a powerful tool designed to automate connection requests on LinkedIn. This bot helps users expand their professional network by sending connection requests to a targeted audience based on specified search criteria. It can be customized to send personalized connection messages.
+
+:star::star::star::star::star: ---> Star the Repo!
+
+
+
+### ⚠️ Important Notes
+
+This script is a LinkedIn bot that automatically sends connection requests with a custom note to profiles on LinkedIn. It uses the Selenium WebDriver to navigate LinkedIn and interact with the UI elements.
+
+# 🚨🚨🚨 Please use it on your own risk! This is against LinkedIn policy. You may get banned if they catch you! 🚨🚨🚨
+
+## 🚨🚨🚨 **Do not exceed 80 requests per week!**  🚨🚨🚨
+
+### LinkedIn may block your account if you exceed this limit.
+
+P.S. For LinkedIn team - ooops, sorry! I was boring! 
+
+### 🚀 Quick Start Guide
+
+1. **💼 Add My LinkedIn:**  
+   You can connect with me on LinkedIn: [My LinkedIn Profile](https://www.linkedin.com/in/mrbondarenko/)
+
+2. **🔍 Set Up Your Search Link:**  
+   To set up your search link, follow these steps:
+   - Go to [LinkedIn's main page](https://www.linkedin.com/).
+   - In the search bar at the top, type in the keywords relevant to the people you want to connect with (e.g., "Tech Recruiter", "Cloud Engineer").
+   - Press Enter to see the search results.
+   - On the search results page, click on the "People" filter to narrow the results to LinkedIn profiles only.
+   - Copy the URL from your browser's address bar. This URL is your `SEARCH_LINK`.
+   - Open the bot script and find the `SEARCH_LINK` variable. Replace the existing link with the one you copied from LinkedIn.
+
+3. **🎉 Have Fun!**  
+   Start the bot and watch as it expands your network with targeted connection requests.
+
+## 🔧 Features
+
+- **🤖 Automated Connection Requests:** Send connection requests to targeted LinkedIn users.
+- **✉️ Personalized Messages:** Customize the connection request message for a more personalized outreach.
+- **🔎 Search Criteria:** Target connections based on industry, location, job title, and more.
+- **📊 Logging:** Keep track of connection requests and responses for analysis and optimization.
+
+## 🛠 Installation
+
+### 📋 Prerequisites
+
+- [Python 3.8+](https://www.python.org/downloads/)
+- [🌐 Mozilla Firefox](https://www.mozilla.org/en-US/firefox/new/) browser
+- [🦊 Geckodriver download page](https://github.com/mozilla/geckodriver/releases) (Ensure that the Geckodriver version matches your installed version of Firefox)
+
+### 📂 Clone the Repository
+
+```bash
+git clone https://github.com/OfficialCodeVoyage/LinkedIn_Auto_Connector_Bot.git
+cd LinkedIn_Auto_Connector_Bot
+```
+
+### 📦 Install Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+## 🛠️ Configuration
+
+1. 🔐 Set up LinkedIn credentials:
+   Change the ```LINKEDIN_USERNAME``` and ```LINKEDIN_PASSWORD``` in the ```Linkedin_auto_connector_bot.py```.
+
+2. 🔍 Customize Your Search Link:
+   Follow the instructions provided in the Quick Start Guide to generate your LinkedIn search link. Replace the SEARCH_LINK variable in the 
+   ```Linkedin_auto_connector_bot.py``` with your copied search link.
+
+3. 📝 Configure Message Template:
+   Adjust the for ```BASE_CONNECTION_MESSAGE ``` your needs in the ```Linkedin_auto_connector_bot.py```.
+
+   
+## 🚀 Usage
+### 🏃 Running the Bot
+
+```bash
+python linkedin_bot.py
+```
+The bot will log into your LinkedIn account and begin sending connection requests based on the search criteria provided in your SEARCH_LINK.
+
+### 📜 Logging
+Logs of sent connection requests and responses are saved in the logs directory. You can review these logs to analyze the performance of your outreach strategy.
+
+### 🤝 Contributing
+
+Contributions are welcome! Please follow these steps to contribute:
+
+🍴 Fork the repository.
+
+🌿 Create a new branch (git checkout -b feature-branch).
+
+💻 Make your changes.
+
+📝 Commit your changes (git commit -am 'Add new feature').
+
+🚀 Push to the branch (git push origin feature-branch).
+
+📬 Open a pull request.
+
+
+### 📄 License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
+
+### ⚠️ Disclaimer
+This bot is intended for educational and research purposes only. The use of automated bots on LinkedIn may violate LinkedIn's terms of service. Use at your own risk.
+
+### 🛠️ Support
+For any questions or issues, please feel free to reach out via GitHub Issues or Discussions.
+
+### 🙌 Acknowledgements
+🤖 Selenium for web automation
+
+🐍 Python for providing the programming language
+
+🌍 The LinkedIn community for providing a platform to connect professionals worldwide
+
+
+Made with ❤️ by [Pavlo Bondarenko](https://www.linkedin.com/in/mrbondarenko/)
